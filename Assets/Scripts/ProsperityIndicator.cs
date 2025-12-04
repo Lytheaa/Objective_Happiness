5b1c36bfbced06ceb3decdba48b0fd26f57d19e7
Pauline Pacé <pauline.pace97@gmail.com>
2025-12-04 14:50:11 +0100

    ProsperityIndicator
    Add prosperity Indicator

---
+++ Assets/Scripts/ProsperityIndicator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProsperityIndicator : MonoBehaviour
+{
+    [Range(0,100)]
+    private int _maxProsperityPoints = 100;
+    //public int MaxProsperityPoints => _maxProsperityPoints;
+    [Range(0, 100)]
+    [SerializeField] private int _currentProsperityPoints = 100; 
+    //public int CurrentProsperityPoints => _currentProsperityPoints;
+
+    [SerializeField] private Image _prosperityIndicator;
+
+
+    public void AddProsperityPoints(int amount)
+    {
+        _currentProsperityPoints += amount;
+    }
+
+    public void SubstractProsperityPoints(int amount)
+    {
+        _currentProsperityPoints -= amount;
+    }
+
+    public void DisplayProsperityIndicator()
+    {
+        _prosperityIndicator.fillAmount = _currentProsperityPoints / (float)_maxProsperityPoints;
+    }
+
+    private void Update()
+    {
+        DisplayProsperityIndicator();
+    }
+}
