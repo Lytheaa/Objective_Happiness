e4482eecb3e6bd9d5001e8b4b85dbc781b5314ea
Pauline Pacé <pauline.pace97@gmail.com>
2025-12-04 11:42:25 +0100

    Player controls

---
+++ Assets/Scripts/PlayerControls/MouseManager.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseManager : MonoBehaviour
+{
+    private Vector2 _pointerPosition;
+    private bool _isPointerDown;
+    private Camera _camera;
+
+
+    [Header("Zoom parameters :")]
+    [SerializeField] private float _minCameraZoom;
+    [SerializeField] private float _maxCameraZoom;
+    [SerializeField] private float _zoomSpeed;
+
+    [Header("Side scrolling parameters :")]
+    [SerializeField] private float _zoneScrollSize;
+    [SerializeField] private float _scrollSpeed;
+
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    public void Update()
+    {
+        if (_isPointerDown)
+        {
+            Debug.Log("click");
+        }
+    }
+
+    public void PointerPositionInputCallback(InputAction.CallbackContext context)
+    {
+        _pointerPosition = context.ReadValue<Vector2>();
+
+        RaycastHit hit; ///RayCast from Camera
+        Ray ray = _camera.ScreenPointToRay(_pointerPosition);
+
+
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider != null)
+            {
+                Debug.Log($"objet touché : {hit.collider.gameObject.name}");
+            }
+        }
+
+        //// SCROLL SCREEN : 
+        if (_pointerPosition.x <= _zoneScrollSize)  // Left
+        {
+            _camera.transform.position -= _camera.transform.right * _scrollSpeed;
+        }
+
+        if (_pointerPosition.x >= Screen.width - _zoneScrollSize) /// Right
+        {
+            _camera.transform.position += _camera.transform.right * _scrollSpeed;
+        }
+
+        if (_pointerPosition.y <= _zoneScrollSize) //Down 
+        {
+            _camera.transform.position -= _camera.transform.up * _scrollSpeed;
+        }
+
+        if (_pointerPosition.y >= Screen.height - _zoneScrollSize) // Up
+        {
+            _camera.transform.position += _camera.transform.up * _scrollSpeed;
+        }
+    }
+       
+    public void ClickCallBack(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            _isPointerDown = true;
+        }
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            _isPointerDown = false;
+        }
+    }
+
+    public void ScrollCallBack(InputAction.CallbackContext context)
+    {
+        float scrollValue = context.ReadValue<float>();
+
+        if (_camera.orthographicSize < _minCameraZoom)
+        {
+            _camera.orthographicSize = _minCameraZoom;
+        }
+        else if (_camera.orthographicSize > _maxCameraZoom)
+        {
+            _camera.orthographicSize = _maxCameraZoom;
+        }
+        else if (_camera.orthographicSize >= _minCameraZoom && _camera.orthographicSize <= _maxCameraZoom)
+        {
+            if (scrollValue > 0)
+            {
+                _camera.orthographicSize -= _camera.orthographicSize * _zoomSpeed;
+            }
+            else if (scrollValue < 0)
+            {
+                _camera.orthographicSize += _camera.orthographicSize * _zoomSpeed;
+            }
+        }
+    }
+
+}
+
+
