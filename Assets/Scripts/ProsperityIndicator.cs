be6d3b900593ddb4c981f6472f395bed89707ab7
Pauline Pacé <pauline.pace97@gmail.com>
2025-12-05 11:34:24 +0100

    Revert "nettoyage Prosperity Indicator"
    This reverts commit faa5402d02cc2687d96778a91a102acbbdacc3d2.

--- Assets/Scripts/ProsperityIndicator.cs
+++ Assets/Scripts/ProsperityIndicator.cs
@@ -18,15 +18,41 @@ public class ProsperityIndicator : MonoBehaviour
     public void AddProsperityPoints(int amount)
     {
         _currentProsperityPoints += amount;
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
         DisplayProsperityIndicator();
 
+>>>>>>> Stashed changes
+=======
+        DisplayProsperityIndicator();
+
+>>>>>>> Stashed changes
+=======
+        DisplayProsperityIndicator();
+
+>>>>>>> Stashed changes
     }
 
     public void SubstractProsperityPoints(int amount)
     {
         _currentProsperityPoints -= amount;
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
         DisplayProsperityIndicator();
 
+>>>>>>> Stashed changes
+=======
+        DisplayProsperityIndicator();
+
+>>>>>>> Stashed changes
+=======
+        DisplayProsperityIndicator();
+
+>>>>>>> Stashed changes
     }
 
     public void DisplayProsperityIndicator()
@@ -34,9 +60,27 @@ public class ProsperityIndicator : MonoBehaviour
         _prosperityIndicator.fillAmount = _currentProsperityPoints / (float)_maxProsperityPoints;
     }
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+    private void Update()
+    {
+        DisplayProsperityIndicator();
+    }
+=======
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
     //private void Update()
     //{
     //    DisplayProsperityIndicator();
     //}
-
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 }
