6bed85f88ce6aedb2c9bd4873c93d9a2579c3596
ElliotN <nedelliot84@gmail.com>
2025-12-10 10:32:43 +0100

    #fix UI game

--- Assets/Scripts/UI/ConstructionModeButton.cs
+++ Assets/Scripts/UI/ConstructionModeButton.cs
@@ -8,17 +8,17 @@ using UnityEngine.UI;
 
 public class ConstructionModeButton : MonoBehaviour
 {
-    [SerializeField] private List<Place> buildings;
+    [SerializeField] private List<GameObject> buildings;
     [SerializeField] private GameObject constructionButton;
     [SerializeField] private Transform BuildingsParent;
     
     private void Awake()
     {
-        foreach (Place building in buildings)
+        foreach (GameObject building in buildings)
         {
             var currentButton = Instantiate(constructionButton, transform);
             var buttonComp = currentButton.GetComponent<Button>();
-            buttonComp.onClick.AddListener(delegate {  });
+            buttonComp.onClick.AddListener(delegate { BuyBuildings(building.GetComponent<Place>()); });
             buttonComp.onClick.AddListener(delegate { transform.gameObject.SetActive(false); });
             currentButton.gameObject.GetComponentsInChildren<Image>()[1].sprite = building.GetComponent<InConstruction>().PreviewSprite;
         }
