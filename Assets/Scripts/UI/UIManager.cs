a226d5456c97b8f4335d4354946d7a3aa341e6ac
Pauline Pacé <pauline.pace97@gmail.com>
2025-12-12 13:50:21 +0100

    Villager selected returned

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -6,9 +6,7 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject _schoolMenu;
 
-    private VillagersGraphics _graphicsSelected; // le villageois actuellement sélectionné
-
-    private Villager _villagerSelected;
+    private Villager _villagerSelected; // le villageois actuellement sélectionné
 
 
     private void Awake()
@@ -19,47 +17,11 @@ public class UIManager : MonoBehaviour
     private void OnEnable()
     {
         MouseManager.OnObjectClicked += ReturnVillagerSelected;
-        //MouseManager.OnObjectClicked += SelectVillager;
     }
     private void OnDisable()
     {
         MouseManager.OnObjectClicked -= ReturnVillagerSelected;
-        //MouseManager.OnObjectClicked -= SelectVillager;
-
     }
-
-    ///TEST METHODE RETOURNE VILLAGER SCRIPT 
-
-    //private void SelectVillager(GameObject clickedObject)
-    //{
-    //    Villager villager = clickedObject.GetComponentInParent<Villager>();
-
-    //    // Si on clique sur rien, désélectionne le villageois actuel
-    //    //if (clickedObject == null)
-    //    //{
-    //    //    DeselectCurrentVillager();
-    //    //    _schoolMenu.SetActive(false);
-    //    //    return;
-    //    //}
-
-    //    if (villager != null)
-    //    {
-    //        Debug.Log("villager retourné");
-    //        _villagerSelected = villager;
-    //        //BOOL sur villager Data ? Is Selected? 
-
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("Impossible de récupérer le villageois");
-    //        _villagerSelected = null;
-    //        ////DeselectCurrentVillager();
-    //        //_schoolMenu.SetActive(false);
-    //        //_villagerSelected = null;
-    //    }
-    //}
-
-    ///METHODE TEST : NE FONCTIONNE PAS 
 
     private void ReturnVillagerSelected(GameObject clickedObject)
     {
@@ -75,99 +37,48 @@ public class UIManager : MonoBehaviour
 
         if (villager != null)
         {
-
-            _villagerSelected = villager;
-
-            Debug.Log("villager retourné");
-            //_villagerSelected.Graphics.IsSelected = true;
-
-            if (_villagerSelected != null && _villagerSelected != villager)
+            // Si on clique sur un villageois différent du précédent
+            if (_villagerSelected != null && _villagerSelected != villager) // Si ce villageois sélectionné est n'a pas le script villager
             {
-                _villagerSelected.Graphics.DisactiveOutline();
                 _villagerSelected.Graphics.IsSelected = false;
+                _villagerSelected.Graphics.DisactiveOutline();
             }
 
-            if (!_villagerSelected.Graphics.IsSelected)
+            //TOGGLE
+            if (!villager.Graphics.IsSelected)
             {
                 _schoolMenu.SetActive(true);
-                _villagerSelected.Graphics.ActiveOutline();
-                _villagerSelected.Graphics.IsSelected = true;
-                _graphicsSelected = _villagerSelected.Graphics;
+                villager.Graphics.ActiveOutline();
+                villager.Graphics.IsSelected = true;
+
+                _villagerSelected = villager;
             }
             else
             {
-                _villagerSelected.Graphics.DisactiveOutline();
-                _villagerSelected.Graphics.IsSelected = false;
-                _graphicsSelected = null;
+                villager.Graphics.DisactiveOutline();
+                villager.Graphics.IsSelected = false;
+                _villagerSelected = null;
                 _schoolMenu.SetActive(false);
             }
         }
         else
         {
-            //DeselectCurrentVillager();
+            DeselectCurrentVillager();
+            _villagerSelected = null;
+
             _schoolMenu.SetActive(false);
             Debug.Log("Impossible de récupérer le villageois");
+        }
+    }
+
+    private void DeselectCurrentVillager()
+    {
+        if (_villagerSelected != null)
+        {
+            _villagerSelected.Graphics.DisactiveOutline();
+            _villagerSelected.Graphics.IsSelected = false;
             _villagerSelected = null;
         }
     }
 
-    /// METHODE GRAPHICS INITIALE : FONCTIONNE 
-
-    //private void GraphicsSelectVillager(GameObject clickedObject)
-    //{
-    //    // Si on clique sur rien, désélectionne le villageois actuel
-    //    if (clickedObject == null)
-    //    {
-    //        DeselectCurrentVillager();
-    //        _schoolMenu.SetActive(false);
-    //        return;
-    //    }
-
-    //    // Récupère le script graphique du villageois cliqué
-    //    VillagersGraphics graphics = clickedObject.GetComponentInParent<VillagersGraphics>();
-
-    //    if (graphics != null)
-    //    {
-    //        // Si on clique sur un villageois différent du précédent
-    //        if (_graphicsSelected != null && _graphicsSelected != graphics)
-    //        {
-    //            _graphicsSelected.DisactiveOutline();
-    //            _graphicsSelected.IsSelected = false;
-    //        }
-
-    //        // Toggle la sélection
-    //        if (!graphics.IsSelected)
-    //        {
-    //            _schoolMenu.SetActive(true);
-    //            graphics.ActiveOutline();
-    //            graphics.IsSelected = true;
-    //            _graphicsSelected = graphics;
-    //        }
-    //        else
-    //        {
-    //            graphics.DisactiveOutline();
-    //            graphics.IsSelected = false;
-    //            _graphicsSelected = null;
-    //            _schoolMenu.SetActive(false);
-    //        }
-    //    }
-    //    else
-    //    {
-    //        // Si on clique sur autre chose que villageois, désélectionne le précédent
-    //        DeselectCurrentVillager();
-    //        _schoolMenu.SetActive(false);
-    //    }
-    //}
-
-    private void DeselectCurrentVillager()
-    {
-        if (_graphicsSelected != null)
-        {
-            _graphicsSelected.DisactiveOutline();
-            _graphicsSelected.IsSelected = false;
-            _graphicsSelected = null;
-        }
-    }
 }
-
-
